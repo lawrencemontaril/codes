405a13a7c25a043c4ea1ec03ad860cd7b64819f3
rrrrence <88632489+rrrrence@users.noreply.github.com>
2021-12-14 19:46:10 +0800

    CSS 6, Performance Task 4
    Source code of CSS 6 Performance Task 4

---
+++ WWP/WWP/Main.java
@@ -0,0 +1,12 @@
+// Author: Montaril
+package WWP;
+
+// Main class.
+public class Main
+{
+	public static void main(String[] args) 
+	{
+		//Quiz quiz = new Quiz();
+		Menu menu = new Menu();
+	}
+}
