3da6d03d8c28b714a51dac969b50bfdfbc7bf42d
rrrrence <88632489+rrrrence@users.noreply.github.com>
2021-12-18 14:40:19 +0800

    Add files via upload

---
+++ WWP/WWP/Main.java
@@ -0,0 +1,18 @@
+// Author: Montaril
+
+// Features: Easy to encode questions and answers, no additional programming.
+// 			 Number of questions are not limited, you can have as many questions as you want.
+//			 Answers are revelead immediately after answering.
+//			 Look and feel are easily customizable, just change the values of the variables!
+
+package WWP;
+
+// Main class.
+public class Main
+{
+	public static void main(String[] args) 
+	{
+		//Quiz quiz = new Quiz();
+		Menu menu = new Menu();
+	}
+}
